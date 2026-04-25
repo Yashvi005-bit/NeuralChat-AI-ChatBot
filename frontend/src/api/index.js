3dce02e585c13187e2cb3cb6b2b6d7fb8bea0d35
Netflix-Clone <yashvishrest@gmail.com>
2026-04-26 02:08:27 +0530

    Fixed cors origin

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://localhost:5000/api/v1",
-  baseURL:"https://neuralchat-ai-chatbot-2.onrender.com",
+  baseURL:"https://neuralchat-ai-chatbot-2.onrender.com/api/v1",
   withCredentials: true
 });
 
