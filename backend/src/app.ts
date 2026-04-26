b38fd031983b5ed3b483108807a80bae60ef9285
Netflix-Clone <yashvishrest@gmail.com>
2026-04-26 10:47:26 +0530

    Fixed cord origin

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,7 +7,8 @@ import cookieParser from "cookie-parser";
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173", // frontend URL , allow request from 5173
+  // origin: "http://localhost:5173", // frontend URL , allow request from 5173
+  origin: "https://neural-chat-ai-chat-bot-4vpy.vercel.app/",
   credentials: true
 }));
 app.use(express.json());
